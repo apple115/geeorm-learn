6e8da85d7b8d7cc634d82e8a5a0be3ccf0ae8a41
apple115 <2567031030@qq.com>
2024-11-09 21:32:20 +0800

    feat: 完成第一章的学习

---
+++ session/raw.go
@@ -0,0 +1,64 @@
+package session
+
+import (
+	"database/sql"
+	"geeorm/log"
+	"strings"
+)
+
+type Session struct {
+	db      *sql.DB
+	sql     strings.Builder
+	sqlVars []interface{}
+}
+
+func New(db *sql.DB) *Session {
+	return &Session{db: db}
+}
+
+func (s *Session) Clear() {
+	s.sql.Reset()
+	s.sqlVars = nil
+}
+
+// DB ...
+func (s *Session) DB() *sql.DB {
+	return s.db
+}
+
+
+// Exec ...
+func (s *Session) Exec() (result sql.Result, err error) {
+	defer s.Clear()
+	log.Info(s.sql.String(), s.sqlVars)
+	if result, err = s.DB().Exec(s.sql.String(), s.sqlVars...); err != nil {
+		log.Error(err)
+	}
+	return
+}
+
+// QueryRow
+func (s *Session) QueryRow() *sql.Row {
+	defer s.Clear()
+	log.Info(s.sql.String(), s.sqlVars)
+	return s.DB().QueryRow(s.sql.String(), s.sqlVars...)
+}
+
+// QueryRows ...
+func (s *Session) QueryRows() (rows *sql.Rows, err error) {
+	defer s.Clear()
+	log.Info(s.sql.String(), s.sqlVars)
+	if rows, err = s.DB().Query(s.sql.String(), s.sqlVars...); err != nil {
+		log.Error(err)
+	}
+	return
+}
+
+
+// Raw ...
+func (s *Session) Raw(sql string, values ...interface{}) *Session {
+	s.sql.WriteString(sql)
+	s.sql.WriteString(" ")
+	s.sqlVars = append(s.sqlVars, values...)
+	return s
+}
